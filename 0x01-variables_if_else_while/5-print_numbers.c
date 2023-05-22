77f29ea88268da63e6af6c88c91a84664daff42c
imanebalou <imanebalouiti@gmail.com>
2023-05-22 13:52:35 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * main - Program that prints all single digit numbers of base 10 starting from 0
  * followed by a new line
- * return (0) Success
+ * Return: (0) Success
  */
 int main(void)
 {
