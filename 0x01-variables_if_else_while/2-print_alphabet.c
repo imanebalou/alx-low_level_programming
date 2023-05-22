b7049d1c21012cbfe1fb5f7f65a7f2d580b238f2
imanebalou <imanebalouiti@gmail.com>
2023-05-22 13:19:20 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * main - Program that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: (0) Success
+ */
+int main(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alph[i]);
+}
+putchar('\n');
+Return (0);
+}
