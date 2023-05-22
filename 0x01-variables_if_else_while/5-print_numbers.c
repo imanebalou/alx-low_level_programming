b69ea0a88f4b37729e0e1731fa891b7ffe7dcd68
imanebalou <imanebalouiti@gmail.com>
2023-05-22 13:47:08 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ * return (0) Success
+ */
+int main(void)
+{
+int i;
+for (i = 48; i =< 57; i++)
+{
+putchar(i);
+}
+putchar('\n')
+return (0);
+}
