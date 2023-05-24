4d6b2d9323609b8efc3c4326babe9a4ba66e0e58
imanebalou <imanebalouiti@gmail.com>
2023-05-24 14:47:19 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that print all single digit number of base10starting from 0
+ * followed by new line
+ * Return: 0 Success
+ **/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
