a3907585abda544af9159b2bc82a449ac905f18d
imanebalou <imanebalouiti@gmail.com>
2023-06-01 12:52:31 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#includ "main.h"
+/**
+ * print_alphabet_x10 - Function that prints 10 times the alphabet
+ * in lowercase
+ * followed by a neww line
+ * Return: 0 Success
+ */
+void print_alphabet_x10(void)
+{
+char apl_10;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (alp_10 = 'a'; alp_10 <= 'z'; alp_10++)
+_putchar(alp_10);
+_putchar('\n');
+}
+}
