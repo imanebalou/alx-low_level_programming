cde8741dca11dcac77bf954800c6a66af73431e7
imanebalou <imanebalouiti@gmail.com>
2023-06-04 08:35:15 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,23 +12,23 @@ for (j = 0; j < 10; j++)
 k = i * j;
 if (k < 10)
 {
-_putchar(k + '0');
 if (j != 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
+_putchar(k + '0');
 }
 else
 {
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
 if (j != 9)
 {
 _putchar(',');
 _putchar(' ');
 }
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
