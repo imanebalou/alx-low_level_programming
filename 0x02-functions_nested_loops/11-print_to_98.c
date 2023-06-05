e6dce2fadeb63993231375ae6de5de93d17c0772
imanebalou <imanebalouiti@gmail.com>
2023-06-05 12:34:23 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,14 @@
 /**
  * print_to_98 - Function that prints all natural numbers from n to 98
  * followed by a new line
+ * @n: the natural number
  */
 void print_to_98(int n)
 {
 int i;
-for(i = n; i < 99; i++)
+for (i = n; i < 99; i++)
 {
-printf("%d",i);
+printf("%d", i);
 if (i != 98)
 {
 _putchar(',');
