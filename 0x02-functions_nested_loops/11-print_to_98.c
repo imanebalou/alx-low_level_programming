4d070b1b7e3c66cbf591824de5107042df59e607
imanebalou <imanebalouiti@gmail.com>
2023-06-06 05:12:21 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ for (i = n; i <= 98; i++)
 {
 if (i != 98)
 {
-printf("%d", i);
+printf("%d",i);
 printf(",");
 printf(" ");
 }
@@ -30,13 +30,13 @@ for (j = n; j >= 98; j--)
 {
 if (j != 98)
 {
-printf("%d", j);
+printf("%d",j);
 printf(",");
 printf(" ");
 }
 else if (j == 98)
 {
-printf("%d\n", i);
+printf("%d\n",i);
 }
 }
 }
